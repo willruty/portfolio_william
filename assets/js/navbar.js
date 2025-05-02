0d0b39e77084b0850437c27c196050661e0a97a2
williamnajatec <william.najatec@gmail.com>
2025-05-02 17:39:56 -0300

    first commit

---
+++ assets/js/navbar.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function () {
+    const bloco = document.getElementById('navbar');
+    const scrollTrigger = 10; // altura em px a partir do topo
+
+    if (window.scrollY > scrollTrigger) {
+        bloco.classList.add('ativo');
+    } else {
+        bloco.classList.remove('ativo');
+    }
+});
